937580c1c3474d292e0b325872f187bab40c1bcf
MateoB00 <mateobianco91100@gmail.com>
2023-06-08 11:14:35 +0200

    [BK]Api-05: send email for each user who have his birthday today

--- api/src/Controllers/userController.js
+++ api/src/Controllers/userController.js
@@ -28,32 +28,25 @@ exports.sendEmail = (req, res) => {
           pass: '4f28b98ae8e89e',
         },
       });
-
-    const users = [
-        {
-          prenom: 'Ariel',
-          nom: 'Ngoualem',
-          email: 'Arielngoualem@gmail.com',
-        },
-      ];
-
-      users.forEach((user) => {
-        const message = {
-          from: 'mbdcbl91@gmail.com', // Adresse e-mail de l'expéditeur
-          to: user.email, // Adresse e-mail du destinataire
-          subject: 'Joyeux anniversaire !', // Sujet de l'e-mail
-          text: `Bonjour ${user.prenom} ${user.nom},\n\nJoyeux anniversaire !`, // Corps de l'e-mail au format texte
-          html: `<p>Bonjour ${user.prenom} ${user.nom},</p><p>Joyeux anniversaire !</p>`, // Corps de l'e-mail au format HTML
-        };
-
-    transporter.sendMail(message, (error, info) => {
-        if (error) {
-            console.log('Erreur lors de l\'envoi de l\'e-mail :', error);
-        } else {
-            console.log('E-mail envoyé :', info.response);
-        }
-        });
-    });
-
-    res.send('E-mails d\'anniversaire envoyés');
+    User.allWhereBirthdayAtToday((results) => {
+        results.forEach((user) => {
+            const message = {
+                from: 'mbscbl91@gmail.com', 
+                to: user.email,
+                subject: 'Joyeux anniversaire !',
+                text: `Bonjour ${user.firstName} ${user.lastName},\n\nJoyeux anniversaire !`, 
+                html: `<p>Bonjour ${user.firstName} ${user.lastName},</p><p>Joyeux anniversaire !</p>`, 
+              };
+      
+          transporter.sendMail(message, (error, info) => {
+              if (error) {
+                  console.log('Erreur lors de l\'envoi de l\'e-mail :', error);
+              } else {
+                  console.log('E-mail envoyé :', info.response);
+              }
+              });
+          });
+      
+          res.send('E-mails d\'anniversaire envoyés');
+    })
 }
