20bae5344263103338222b4fef303cb128fbcd35
MateoB00 <mateobianco91100@gmail.com>
2023-06-08 11:19:11 +0200

    [BK]Api-06: check validate birthdayDate for format date in db

--- api/src/Services/csv-to-db.js
+++ api/src/Services/csv-to-db.js
@@ -18,19 +18,22 @@ exports.UploadCsvDataToMySQL = (filePath) => {
                 const user = csvData[i][0]
                 const champsUser = user.split(';');
                 // console.log(champsUser)
-                // if (date = '10/02/2022'){
-                //     faire ça sinon nn
-                // }
+           
                 console.log(champsUser[0])
                 const rawDate = champsUser[3];
-                const day = rawDate.substring(0, 2);
-                const month = rawDate.substring(3, 5);
-                const year = rawDate.substring(6, 10);
-                const formattedDate = `${year}-${month}-${day}`;
-
-                User.create(champsUser[0], champsUser[1], champsUser[2], formattedDate, (result) => {
-                    res.json(result);
-                })
+                if (rawDate.includes('/')){
+                    const day = rawDate.substring(0, 2);
+                    const month = rawDate.substring(3, 5);
+                    const year = rawDate.substring(6, 10);
+                    let formattedDate = `${year}-${month}-${day}`;    
+                    User.create(champsUser[0], champsUser[1], champsUser[2], formattedDate, (result) => {
+                        res.json(result);
+                    })
+                } else {
+                    User.create(champsUser[0], champsUser[1], champsUser[2], rawDate, (result) => {
+                        res.json(result);
+                    })
+                }
             }
 
             // delete file after saving to MySQL database
