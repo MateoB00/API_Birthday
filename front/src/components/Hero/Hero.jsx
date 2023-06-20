43a577d393fc978d5049044f84dfe198afd43869
jouvenceautanguy <tanguy.jouvenceau@my-digital-school.org>
2023-06-20 15:21:30 +0200

    Ajustements & couleurs

--- front/src/components/Hero/Hero.jsx
+++ front/src/components/Hero/Hero.jsx
@@ -8,42 +8,36 @@ import Character4 from "../../assets/character_4.svg";
 import "./Hero.css";
 
 function Hero() {
+
+  // Variables
   const [datas, setDatas] = useState([]);
   const [colors, setColors] = useState("#df80ac");
-
-const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
-  useEffect(() => {
-    fetch("/all") // Remplacez "/pays" avec l'URL correcte pour la route de récupération des options
-      .then((response) => response.json())
-      .then((data) => {
-        setDatas(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching options:", error);
-      });
-  }, []);
-
-  function selectRandomCharacter() {
-    const characters = [Character1, Character2, Character3, Character4];
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    const selectedCharacter = characters[randomIndex];
-    return selectedCharacter;
-  }
-  function getRandomColor() {
-    const nombersColors = toutesCouleurs.length;
-    return Math.floor(Math.random() * nombersColors);
-  }
   const nombre1 = "1";
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const [filled, setFilled] = useState(0);
   const [isRunning, setIsRunning] = useState(true);
-  const [Character, setCharacter] = useState(selectRandomCharacter());
+  const [Character, setCharacter] = useState(Character1);
   const [prenom, setPrenom] = useState("");
   const [nom, setNom] = useState("");
   const [nombre, setNombre] = useState("");
   const [isBumping, setIsBumping] = useState(false);
+  const [colorIndex, setColorIndex] = useState(0);
+  const toutesCouleurs = ["#098E27", "#579FF4", "#df80ac", "#FCB325"];
 
+// fonctions Randoms
+function getCharacters() {
+  const characters = [Character3, Character2, Character1, Character4];
+  const numberOfColors = toutesCouleurs.length;
+  const characterIndex = colorIndex % characters.length;
+  return characters[characterIndex];
+}
+  function getRandomColor() {
+    const numberOfColors = toutesCouleurs.length;
+    const currentColor = toutesCouleurs[colorIndex];
+    setColorIndex((colorIndex + 1) % numberOfColors);
+    return currentColor;
+  }
+// UseEffect pour l'animation des étoiles
   useEffect(() => {
     const bumpInterval = setInterval(() => {
       setIsBumping(true);
@@ -51,12 +45,12 @@ const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
         setIsBumping(false);
       }, 500);
     }, 5000);
-  
+
     return () => {
       clearInterval(bumpInterval);
     };
   }, []);
-
+  // Fetch API
   useEffect(() => {
     fetch("/all")
       .then((response) => response.json())
@@ -67,29 +61,26 @@ const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
           setPrenom(data[0]._firstName);
           setNom(data[0]._lastName);
           setCurrentIndex(1);
-          
         }
-    
       })
       .catch((error) => {
         console.error("Error fetching options:", error);
       });
   }, []);
 
+// Barre de progression
   useEffect(() => {
     if (filled < 100 && isRunning) {
       setTimeout(() => {
         setFilled((prev) => {
           const newFilled = prev + 1;
-          console.log(prev);
           if (newFilled >= 100) {
-            setCharacter(selectRandomCharacter());
+            setCharacter(getCharacters());
             if (datas.length > 0) {
               setPrenom(datas[currentIndex % datas.length]._firstName);
               setNom(datas[currentIndex % datas.length]._lastName);
             }
-            setColors(toutesCouleurs[getRandomColor()]);
-            console.log("nimporre quoi")
+            setColors(getRandomColor());
             setCurrentIndex((prevIndex) => prevIndex + 1);
             setTimeout(() => {
               setFilled(0);
@@ -106,13 +97,25 @@ const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
     <div className="hero">
       <div className="left-container" style={{ backgroundColor: colors }}>
         <div className="star1">
-          <img src={Star} alt="Mon Logo" className={`stars1 ${isBumping ? "bump" : ""}`} />
+          <img
+            src={Star}
+            alt="Mon Logo"
+            className={`stars1 ${isBumping ? "bump" : ""}`}
+          />
         </div>
         <div className="star2">
-          <img src={Star} alt="Mon Logo" className={`stars2 ${isBumping ? "bump" : ""}`} />
+          <img
+            src={Star}
+            alt="Mon Logo"
+            className={`stars2 ${isBumping ? "bump" : ""}`}
+          />
         </div>
         <div className="star3">
-          <img src={Star} alt="Mon Logo" className={`stars3 ${isBumping ? "bump" : ""}`} />
+          <img
+            src={Star}
+            alt="Mon Logo"
+            className={`stars3 ${isBumping ? "bump" : ""}`}
+          />
         </div>
         <div className="image1">
           <img src={Logo} alt="Mon Logo" className="rotate-image" />
@@ -123,7 +126,10 @@ const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
         </p>
       </div>
       <div className="right-container">
-        <div className="card-text" style={{ boxShadow:` 10px 10px 0px ${colors}` }}>
+        <div
+          className="card-text"
+          style={{ boxShadow: ` 10px 10px 0px ${colors}` }}
+        >
           <p className="p1">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
             maiores recusandae pariatur, explicabo id vel nobis? Eaque, harum
@@ -132,7 +138,10 @@ const toutesCouleurs = ["#098E27","#579FF4","#df80ac","#FCB325"]
           </p>
           <p className="p2"> - Michael Jackson</p>
         </div>
-        <div className="card-characters" style={{ boxShadow:` 10px 10px 0px ${colors}` }}>
+        <div
+          className="card-characters"
+          style={{ boxShadow: ` 10px 10px 0px ${colors}` }}
+        >
           <img src={Character} alt="Mon personnage" />
           {/* condition to display the character */}
         </div>
